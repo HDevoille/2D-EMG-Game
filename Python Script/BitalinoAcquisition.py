882108c3b52131e92fa5f7d24e92d19b4fffbf07
HDevoille <113013403+HDevoille@users.noreply.github.com>
2023-06-13 14:28:30 +0200

    Update BitalinoAcquisition.py

--- Python Script/BitalinoAcquisition.py
+++ Python Script/BitalinoAcquisition.py
@@ -5,19 +5,22 @@ import threading
 
 class GUI:
 
-    def __init__(self,inlet):
-        self.inlet = inlet
+    def __init__(self):
+        self.inlet = None
         self.max_strength = 0 
         self.root = tk.Tk()
         
-        self.root.geometry("300x300")
-        self.root.title("Bitalino Acquisition")
+        self.root.geometry("500x300")
+        self.root.title("2D EMG Game Acquisition")
 
         self.label = tk.Label(self.root,text = "Console :")
         self.label.pack(padx=20,pady=10)
 
         self.console = tk.Text(self.root,height=6)
         self.console.pack(padx=20,pady=10)
+
+        self.buttonFindStream = tk.Button(self.root,text="Find Stream",command=threading.Thread(target=self.open_stream).start)
+        self.buttonFindStream.pack(padx=20,pady=10)
 
         self.buttonCalibration = tk.Button(self.root,text="Launch Calibration",command = threading.Thread(target=self.calibration).start)
         self.buttonCalibration.pack(padx=20,pady=10)
@@ -33,6 +36,7 @@ class GUI:
         return abs(sample[1])
 
     def calibration(self):
+        self.console.delete(1.0,tk.END)
         self.console.insert(tk.END,"Squeeze to full strength \n")
         n = 0
         calibration_data = []
@@ -50,6 +54,7 @@ class GUI:
             return last_sample
 
     def start_server(self):
+        self.console.delete(1.0,tk.END)
         # Créer un socket
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
@@ -77,65 +82,66 @@ class GUI:
             # Fermer le socket client
             client_socket.close()
 
-def open_stream():
-    print("Looking for OpenSignals stream ...")
-    os_stream = resolve_stream("name","OpenSignals")
-    inlet = StreamInlet(os_stream[0])
-    return inlet
+    def open_stream(self):
+        self.console.delete(1.0,tk.END)
+        self.console.insert(tk.END,"Looking for OpenSignals stream ...")
+        os_stream = resolve_stream("name","OpenSignals")
+        self.inlet = StreamInlet(os_stream[0])
 
-# def recv_data(inlet):
-#         sample,timestamp = inlet.pull_sample()
-#         return abs(sample[1])
+"""
+def recv_data(inlet):
+        sample,timestamp = inlet.pull_sample()
+        return abs(sample[1])
 
-# def calibration(inlet):
-#     n = 0
-#     calibration_data = []
-#     while n < 1000:
-#         sample = recv_data(inlet)
-#         calibration_data.append(sample)
-#         n+=1
-#     maxstrength = max(calibration_data)
-#     print(maxstrength)
-#     return maxstrength
+def calibration(inlet):
+    n = 0
+    calibration_data = []
+    while n < 1000:
+        sample = recv_data(inlet)
+        calibration_data.append(sample)
+        n+=1
+    maxstrength = max(calibration_data)
+    print(maxstrength)
+    return maxstrength
 
-# def traitement_data(maxstrength,inlet):
-#     last_sample = recv_data(inlet)
-#     if last_sample > maxstrength/2:
-#         return last_sample
+def traitement_data(maxstrength,inlet):
+    last_sample = recv_data(inlet)
+    if last_sample > maxstrength/2:
+        return last_sample
 
-# def start_server(maxstrength,inlet):
-#     # Créer un socket
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_server(maxstrength,inlet):
+    # Créer un socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      
-#     # Attacher le socket à une adresse et un port spécifiques
-#     server_socket.bind(('localhost', 12345))
+    # Attacher le socket à une adresse et un port spécifiques
+    server_socket.bind(('localhost', 12345))
         
-#     # Commencer à écouter pour les connexions entrantes
-#     server_socket.listen(1)
-#     #self.console.insert(tk.END,"Server started and listening on localhost:12345")
-#     print("Server Started")
+    # Commencer à écouter pour les connexions entrantes
+    server_socket.listen(1)
+    #self.console.insert(tk.END,"Server started and listening on localhost:12345")
+    print("Server Started")
 
-#     while True:
-#         # Accepter une connexion entrante
-#         client_socket, addr = server_socket.accept()
-#         #self.console.insert(tk.END,f"Connection from {addr} has been established!")
-#         print(f"Connection from {addr} has been established!")
+    while True:
+        # Accepter une connexion entrante
+        client_socket, addr = server_socket.accept()
+        #self.console.insert(tk.END,f"Connection from {addr} has been established!")
+        print(f"Connection from {addr} has been established!")
 
-#         while True:
-#             try:
-#             # Envoyer un message au client
-#                 client_socket.send(bytes(str(traitement_data(maxstrength,inlet))+",", "utf-8"))
-#             except:
-#                 #self.console.insert(tk.END,"Client has disconnected")
-#                 print("Client disconnected")
-#                 break  # sortir de la boucle interne si le client est déconnecté
+        while True:
+            try:
+            # Envoyer un message au client
+                client_socket.send(bytes(str(traitement_data(maxstrength,inlet))+",", "utf-8"))
+            except:
+                #self.console.insert(tk.END,"Client has disconnected")
+                print("Client disconnected")
+                break  # sortir de la boucle interne si le client est déconnecté
 
-#         # Fermer le socket client
-#         client_socket.close()
+        # Fermer le socket client
+        client_socket.close()
+"""
 
 if __name__ == "__main__":
-    inlet = open_stream()
-    GUI(inlet)
+    GUI()
 
     # inlet = open_stream()
     # maxstrength = calibration(inlet)
